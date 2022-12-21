32d2f903d7544471dfa57b10c802f4720045ca53
Marina Ionel <marinicaionel@gmail.com>
2022-12-21 12:34:47 +0100

    env setup
    generate env files dynamically

---
+++ scripts/setenv.js
@@ -0,0 +1,25 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+
+require('dotenv').config();
+
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+const targetPath = isProduction
+    ? `./src/environments/environment.prod.ts`
+    : `./src/environments/environment.ts`;
+
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   weatherApiBaseUrl: "${process.env.WEATHER_API_BASE_URL}",
+   weatherApiKey: "${process.env.WEATHER_API_KEY}"
+};
+`;
+
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err) {
+    if (err) console.log(err);
+    console.log(`Wrote variables to ${targetPath}`);
+})
