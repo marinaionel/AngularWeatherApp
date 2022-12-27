20adddd77c9f9541f8cc95821a0b6276c0313658
Marina Ionel <marinicaionel@gmail.com>
2022-12-27 13:36:41 +0100

    fixed failing render test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularWeatherApp app is running!');
+    expect(compiled.querySelector('.container a h1')?.textContent).toContain('Angular Weather App');
   });
 });
